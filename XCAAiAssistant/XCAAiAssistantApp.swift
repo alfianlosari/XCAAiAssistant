c8660e84919e2c95585aca699783fd46454ec7e8
Alfian Losari <alfianlosari@gmail.com>
2023-11-26 14:58:16 +0700

    Initial Commit

---
+++ XCAAiAssistant/XCAAiAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCAAiAssistantApp.swift
+//  XCAAiAssistant
+//
+//  Created by Alfian Losari on 26/11/23.
+//
+
+import SwiftUI
+
+@main
+struct XCAAiAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
